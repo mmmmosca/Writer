e749a49230bfc1c1f1588be679976ef9285945c9
Fly <69603387+mmmmosca@users.noreply.github.com>
2023-05-16 18:20:01 +0200

    Update decypher.py

--- Writer/decypher.py
+++ Writer/decypher.py
@@ -1,6 +1,6 @@
 import sympy
 
-array = input("Enter numeric values ​​as written separated by spaces or commas (don't remove numbers with commas from numbers):").split()
+array = []
 
 array = [num if isinstance(num, int) else int(num) if num.is_integer() else float(f'{num:.2f}') for num in array]
 		
