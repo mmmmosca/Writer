a98ac5231302c24b25a949a03e21e61b5aaa7d45
Fly <69603387+mmmmosca@users.noreply.github.com>
2023-05-17 16:24:08 +0200

    Update decypher.py

--- Writer/decypher.py
+++ Writer/decypher.py
@@ -1,7 +1,5 @@
 array = eval(input("Enter the array: "))
 
-array = [num if isinstance(num, int) else int(num) if num.is_integer() else float(f'{num:.2f}') for num in array] 
-                  
 for i in range(len(array)): 
 	array[i] = array[i]*2 
 	array[i] = int(array[i]) 
