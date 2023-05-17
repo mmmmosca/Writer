1abab5f3fbf1ef7450c953fd08e7ab1d503283b1
Fly <69603387+mmmmosca@users.noreply.github.com>
2023-05-17 09:58:22 +0200

    Update decypher.py

--- Writer/decypher.py
+++ Writer/decypher.py
@@ -1,19 +1,15 @@
-import sympy
+import sympy 
 
-array = []
+array = [56.0, 57.0, 55.5, 59.0, 48.5]
 
-array = [num if isinstance(num, int) else int(num) if num.is_integer() else float(f'{num:.2f}') for num in array]
-		
+array = [num if isinstance(num, int) else int(num) if num.is_integer() else float(f'{num:.2f}') for num in array] 
+                  
+for i in range(len(array)): 
+	array[i] = array[i]*2 
+	array[i] = int(array[i]) 
 
-for i in range(len(array)):
-    if sympy.isprime(array[i]):
-    	array[i] = int(array[i])
-    else:
-    	array[i] = array[i]*2
-    	array[i] = int(array[i])
-    	
-def decrypt(array):
-	for char in array:
-		print(chr(char),end='')
+def decrypt(array): 
+	for char in array: 
+		print(chr(char),end='') 
 
 decrypt(array)
