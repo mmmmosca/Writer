839351cd7040c7937196e62676e0c628a8878c8c
Fly <69603387+mmmmosca@users.noreply.github.com>
2023-05-18 09:05:35 +0200

    Update decypher.py

--- Writer/decypher.py
+++ Writer/decypher.py
@@ -1,5 +1,10 @@
+import os
 array = eval(input("Enter the array: "))
-
+if os.name == 'nt':
+        os.system('cls')  # Per Windows
+    else:
+        os.system('clear')
+         
 for i in range(len(array)): 
 	array[i] = array[i]*2 
 	array[i] = int(array[i]) 
