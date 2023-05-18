c99a3ea867f912081b63dc35bab0d7a112e667d3
Fly <69603387+mmmmosca@users.noreply.github.com>
2023-05-18 09:07:27 +0200

    Update cypher.py

--- Writer/cypher.py
+++ Writer/cypher.py
@@ -24,7 +24,7 @@ def cypher(text):
 
 text = input()
 if os.name == 'nt':
-        os.system('cls')  # Per Windows
-    else:
-        os.system('clear')
+	os.system('cls')  # Per Windows
+else:
+	os.system('clear')
 cypher(text)
